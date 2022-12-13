a9499a7c356c0591db8cf3f4156533873d97d2de
Doriham Russo <doriham123@gmail.com>
2022-12-13 15:17:06 -0300

    add prop and proptypes to component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header></Header>
+      <Header title={2}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <header>
-        <h1>Task Tracker</h1>  
+        <h1>{title}</h1>  
     </header>
   )
+}
+
+Header.defaultProps = {
+    title : "Task Tracker"
+}
+
+Header.propTypes = {
+    title : PropTypes.string,
+    
 }
 
 export default Header
