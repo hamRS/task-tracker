fd23d438b0b485fea5954fe99c271c794b49837b
Doriham Russo <doriham123@gmail.com>
2022-12-19 12:31:11 -0300

    implementing basic useState hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types'
 
 import Button from './Button'
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log("clicked!")
+  }
   return (
     <header className="header">
         <h1>{title}</h1>
-        <Button text="Add"/>
+        <Button text="Add" color="green" onClick={onClick}/>
     </header>
   )
 }
